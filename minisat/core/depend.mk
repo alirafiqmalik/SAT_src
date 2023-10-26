8558268e4555c358bcb68b61724fe29149425aea
alirafiqmalik <amalik.bee19seecs@seecs.edu.pk>
2023-10-26 03:26:10 -0300

    minisat committed

---
+++ minisat/core/depend.mk
@@ -0,0 +1,11 @@
+/mnt/d/alis_files/LAPTOP/alis_files/university_files/PROJECTS_2022-2023/FYP/linux/LL_Attacks/SAT_src/minisat/core/Solver.o /mnt/d/alis_files/LAPTOP/alis_files/university_files/PROJECTS_2022-2023/FYP/linux/LL_Attacks/SAT_src/minisat/core/Solver.or /mnt/d/alis_files/LAPTOP/alis_files/university_files/PROJECTS_2022-2023/FYP/linux/LL_Attacks/SAT_src/minisat/core/Solver.od /mnt/d/alis_files/LAPTOP/alis_files/university_files/PROJECTS_2022-2023/FYP/linux/LL_Attacks/SAT_src/minisat/core/Solver.op: \
+ /mnt/d/alis_files/LAPTOP/alis_files/university_files/PROJECTS_2022-2023/FYP/linux/LL_Attacks/SAT_src/minisat/core/Solver.cc \
+ ../mtl/Sort.h ../mtl/Vec.h ../mtl/IntTypes.h ../mtl/XAlloc.h \
+ ../core/Solver.h ../mtl/Heap.h ../mtl/Alg.h ../utils/Options.h \
+ ../utils/ParseUtils.h ../core/SolverTypes.h ../mtl/Map.h ../mtl/Alloc.h
+/mnt/d/alis_files/LAPTOP/alis_files/university_files/PROJECTS_2022-2023/FYP/linux/LL_Attacks/SAT_src/minisat/core/Main.o /mnt/d/alis_files/LAPTOP/alis_files/university_files/PROJECTS_2022-2023/FYP/linux/LL_Attacks/SAT_src/minisat/core/Main.or /mnt/d/alis_files/LAPTOP/alis_files/university_files/PROJECTS_2022-2023/FYP/linux/LL_Attacks/SAT_src/minisat/core/Main.od /mnt/d/alis_files/LAPTOP/alis_files/university_files/PROJECTS_2022-2023/FYP/linux/LL_Attacks/SAT_src/minisat/core/Main.op: \
+ /mnt/d/alis_files/LAPTOP/alis_files/university_files/PROJECTS_2022-2023/FYP/linux/LL_Attacks/SAT_src/minisat/core/Main.cc \
+ ../utils/System.h ../mtl/IntTypes.h ../utils/ParseUtils.h \
+ ../utils/Options.h ../mtl/Vec.h ../mtl/XAlloc.h ../core/Dimacs.h \
+ ../core/SolverTypes.h ../mtl/Alg.h ../mtl/Map.h ../mtl/Alloc.h \
+ ../core/Solver.h ../mtl/Heap.h
